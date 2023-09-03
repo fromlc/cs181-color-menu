d035616c8981a31e0282605fac9ceb505a89c8ea
L Carver <wifihum@gmail.com>
2023-09-03 13:07:01 -0700

    add while loop to run until quit, update comments

--- source/cs181-color-menu.cpp
+++ source/cs181-color-menu.cpp
@@ -14,40 +14,47 @@ using namespace std;
 //------------------------------------------------------------------------------
 // entry point
 //------------------------------------------------------------------------------
-int main()
-{
+int main() {
     // app banner
     cout << "\nPick a console text color!\n\n";
 
-    // get user color choice
     char choice;
-    
-    cout << "R)ed, G)reen, Y)ellow, B)lue, M)agenta, C)yan, W)hite? ";
-    cin >> choice;
-    choice = toupper(choice);
 
-    // set console color to user's choice
-    if (choice == 'R')
-        cout << FG_RED;
-    else if (choice == 'G')
-        cout << FG_GREEN;
-    else if (choice == 'Y')
-        cout << FG_YELLOW;
-    else if (choice == 'B')
-        cout << FG_BLUE;
-    else if (choice == 'M')
-        cout << FG_MAGENTA;
-    else if (choice == 'C')
-        cout << FG_CYAN;
-    else if (choice == 'W')
-        cout << FG_WHITE;
-    else
-        cout << "Sorry, I don't know that color.\n";
+    // display menu until user quits
+    while (true)
+    {
+        // get user color choice in uppercase
+        cout << "R)ed, G)reen, Y)ellow, B)lue, M)agenta, C)yan, W)hite, Q)uit? ";
+        cin >> choice;
+        choice = toupper(choice);
+
+        // terminate app on user quit
+        if (choice == 'Q')
+            break;
+
+        // set console color to user's choice
+        if (choice == 'R')
+            cout << FG_RED;
+        else if (choice == 'G')
+            cout << FG_GREEN;
+        else if (choice == 'Y')
+            cout << FG_YELLOW;
+        else if (choice == 'B')
+            cout << FG_BLUE;
+        else if (choice == 'M')
+            cout << FG_MAGENTA;
+        else if (choice == 'C')
+            cout << FG_CYAN;
+        else if (choice == 'W')
+            cout << FG_WHITE;
+        else
+            cout << "Sorry, I don't know that color.\n";
+    }
 
     // show new console text color
     cout << "Goodbye!\n";
 
     // set console text color back to previous color
     cout << RESET;
-    
+
 }
